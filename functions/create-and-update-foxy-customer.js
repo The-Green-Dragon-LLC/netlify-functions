51b62e3106d428f2de01d351d0a6f47534b566f7
Jesus Perez <jesusmpds18@gmail.com>
2023-05-19 14:43:49 -0300

    Add create-and-update-foxy-customer.js endpoint for creating wholesale users

---
+++ functions/create-and-update-foxy-customer.js
@@ -0,0 +1,76 @@
+import * as FoxySDK from "@foxy.io/sdk";
+const { FOXY_REFRESH_TOKEN, FOXY_CLIENT_SECRET, FOXY_CLIENT_ID } = process.env;
+const customerByEmail = customerEmail =>
+  `https://api.foxycart.com/stores/101277/customers?email=${customerEmail}`;
+const createCustomer = "https://api.foxycart.com/stores/101277/customers";
+const foxy = new FoxySDK.Backend.API({
+  refreshToken: FOXY_REFRESH_TOKEN,
+  clientSecret: FOXY_CLIENT_SECRET,
+  clientId: FOXY_CLIENT_ID,
+});
+
+exports.handler = async (event, context) => {
+  try {
+    const { customer, customer_tier, customer_id } = JSON.parse(event.body);
+    const customerExists = await (await foxy.fetch(customerByEmail(customer.email))).json();
+
+    // Update Tier Flow
+    if (customer_id && customerExists.returned_items) {
+      const customerAttributes = customerExists._links["fx:attributes"].href;
+      const attributes = await (
+        await foxy.fetch(customerAttributes, {
+          method: "PATCH",
+          body: JSON.stringify({ name: "wholesale_tier", value: customer_tier }),
+        })
+      ).json();
+      return {
+        body: JSON.stringify({
+          ok: true,
+          data: {
+            attributes,
+          },
+        }),
+        statusCode: 200,
+      };
+    }
+
+    //Create Customer and Add Tier Flow
+    if (!customerExists.returned_items) {
+      const newCustomer = await (
+        await foxy.fetch(createCustomer, {
+          method: "POST",
+          body: JSON.stringify(customer),
+        })
+      ).json();
+
+      const customerAttributes = customer._links["fx:attributes"].href;
+
+      const attributes = await (
+        await foxy.fetch(customerAttributes, {
+          method: "PATCH",
+          body: JSON.stringify({ name: "wholesale_tier", value: customer_tier }),
+        })
+      ).json();
+
+      return {
+        body: JSON.stringify({
+          ok: true,
+          data: {
+            customer: newCustomer,
+            attributes,
+          },
+        }),
+        statusCode: 200,
+      };
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      body: JSON.stringify({
+        ok: false,
+        details: "An error has occurred when creating the foxy customer",
+      }),
+      statusCode: 500,
+    };
+  }
+};
