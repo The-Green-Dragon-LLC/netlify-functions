f42b6bf218458a0cc80ca5d752538b8b493f811a
Lauren Xie <lrnxie@gmail.com>
2023-06-20 11:56:23 -0400

    Create custom tax endpoint

---
+++ functions/custom-tax-endpoint.js
@@ -0,0 +1,43 @@
+exports.handler = async (event, context) => {
+  try {
+    const payload = JSON.parse(event.body);
+    const totalShipping = payload.total_shipping;
+    const totalItemPrice = payload.total_item_price;
+    const totalDiscount = payload.total_discount;
+
+    const taxRate = totalShipping === 0 ? "0.08238" : "0.08738";
+    const taxAmount =
+      Math.round(
+        taxRate * (totalItemPrice + totalDiscount + totalShipping) * 100
+      ) / 100;
+
+    const taxConfiguration = {
+      ok: true,
+      details: "",
+      name: "Tax",
+      expand_taxes: [
+        {
+          name: "Tax",
+          rate: taxRate,
+          amount: taxAmount,
+        },
+      ],
+      total_amount: taxAmount,
+      total_rate: taxRate,
+    };
+
+    return {
+      body: JSON.stringify(taxConfiguration),
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      body: JSON.stringify({
+        ok: false,
+        details: "An error has occurred when fetching tax rates",
+      }),
+      statusCode: 500,
+    };
+  }
+};
